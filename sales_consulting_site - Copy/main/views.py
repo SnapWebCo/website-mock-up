1ba47db4d6c93696c2ab8ace6b78d7f4f06e129c
SnapWebCo <snapwebco@gmail.com>
2025-07-04 00:48:33 -0400

    Add files via upload

---
+++ sales_consulting_site - Copy/main/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+@login_required
+def dashboard(request):
+    return render(request, 'dashboard.html')
